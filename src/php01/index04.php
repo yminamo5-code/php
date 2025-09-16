4ba7c1d6a592adbd7fd2551dc3faf4908920ab75
jo5to7ko0 <y.minamo5@gmail.com>
2025-09-17 00:10:16 +0900

    リモートリポジトリの変更

---
+++ src/php01/index04.php
@@ -0,0 +1,13 @@
+<?php
+$people = array(
+  'person1' => 'Taro',
+  'person2'  => 'Jiro',
+  'person3'  => 'Saburo'
+);
+
+foreach ($people as $per => $name) {
+  print $pe . "は" . $name . "です" . '<br />';
+}
+
+
+var_dump($people);
